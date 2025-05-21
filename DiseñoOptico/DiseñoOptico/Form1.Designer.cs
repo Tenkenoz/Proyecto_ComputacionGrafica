cb615180a133bb64346328eeec85034bdf202f71
AJ <67701292+Ariel2006@users.noreply.github.com>
2025-05-20 22:27:24 -0500

    Proyecto Gráfica
    Figura con ondas simultaneamente

---
+++ DiseñoOptico/DiseñoOptico/Form1.Designer.cs
@@ -0,0 +1,27 @@
+﻿namespace DiseñoOptico
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Diseño Óptico - Ondas en Figuras";
+            this.ResumeLayout(false);
+        }
+    }
+}
